d082005a4a95c5b21a75addca62a21bece0a1296
Chottek <49697145+Chottek@users.noreply.github.com>
2020-05-14 17:47:14 +0200

    Initial commit of auction services application

---
+++ src/main/java/pl/fox/allezon/AllezonApplication.java
@@ -0,0 +1,13 @@
+package pl.fox.allezon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllezonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllezonApplication.class, args);
+	}
+
+}
